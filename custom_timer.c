4718bd38b7c39a3121babfbaff4c57729d87fa14
Jula <juicy.jelly74@gmail.com>
2016-10-23 17:46:58 -0700

    Changed timer init function

--- custom_timer.c
+++ custom_timer.c
@@ -9,10 +9,10 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Julia Kozel");
 MODULE_VERSION("0.01");
 
-static struct timer_list my_timer;
+static struct timer_list custom_timer;
 static int delay = 7;
 static int times_left = 4;
-static struct kobject *mytimer_kobject;
+static struct kobject *customtimer_kobject;
 
 static void print_text(unsigned long data)
 {
@@ -22,53 +22,53 @@ static void print_text(unsigned long data)
 	} 
 	//jiffies определяет количество временных тиков (тактов)
 	//, прошедших после загрузки системы	
-	mod_timer(&my_timer, jiffies + delay * HZ);
+	mod_timer(&custom_timer, jiffies + delay * HZ);
 }
 
-static ssize_t mytimer_read(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t customtimer_read(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%d\n", times_left);
 }
 
-static ssize_t mytimer_write(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+static ssize_t customtimer_write(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
 	sscanf(buf, "%d", &times_left);
 	return count;
 }
 
-static struct kobj_attribute times_left_attribute =__ATTR(times_left, 0660, mytimer_read, mytimer_write);
+static struct kobj_attribute times_left_attribute =__ATTR(times_left, 0660, customtimer_read, customtimer_write);
 
-static int mytimer_init(void)
+static int customtimer_init(void)
 {
 	int error = 0;	
 
 	//create a kobject and place it in sysfs in the location
 	//underneath the specified parent kobject
-	mytimer_kobject = kobject_create_and_add("mytimer", kernel_kobj);
-	if (!mytimer_kobject) {
+	customtimer_kobject = kobject_create_and_add("customtimer", kernel_kobj);
+	if (!customtimer_kobject) {
 		return -ENOMEM;
 	}
 
 	//associate the attribute structure pointed at by attr 
 	//with the kobject pointed at by kobj
-	error = sysfs_create_file(mytimer_kobject, &times_left_attribute.attr);
+	error = sysfs_create_file(customtimer_kobject, &times_left_attribute.attr);
 	if (error) {
-		printk(KERN_INFO "failed to create the times_left file in /sys/kernel/mytimer \n");
+		printk(KERN_INFO "failed to create the times_left file in /sys/kernel/customtimer \n");
 		return error;
 	}
-	init_timer_on_stack(&my_timer);
-	my_timer.expires = jiffies + delay * HZ;
-	my_timer.data = 0;
-	my_timer.function = print_text;
-	add_timer(&my_timer);
+	init_timer_on_stack(&custom_timer);
+	custom_timer.expires = jiffies + delay * HZ;
+	custom_timer.data = 0;
+	custom_timer.function = print_text;
+	add_timer(&custom_timer);
 	return 0;
 }
 
-static void mytimer_exit(void)
+static void customtimer_exit(void)
 {	
-	kobject_put(mytimer_kobject);
-	del_timer(&my_timer);
+	kobject_put(customtimer_kobject);
+	del_timer(&custom_timer);
 }
 
-module_init(mytimer_init);
-module_exit(mytimer_exit);
+module_init(customtimer_init);
+module_exit(customtimer_exit);
